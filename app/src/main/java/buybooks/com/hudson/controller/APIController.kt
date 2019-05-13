17648b339d9cd5182340b759adb3a68d887a8a34
algebra1415011 <avinashishobha@gmail.com>
2019-05-13 13:05:21 +0530

    making changes

--- app/src/main/java/buybooks/com/hudson/controller/APIController.kt
+++ app/src/main/java/buybooks/com/hudson/controller/APIController.kt
@@ -3,12 +3,15 @@ package buybooks.com.hudson.controller
 import buybooks.com.hudson.ServiceInterface
 import com.android.volley.toolbox.JsonArrayRequest
 import org.json.JSONArray
-import org.json.JSONObject
+import org.json.JSONObject    
 
 class APIController constructor(serviceInjection: ServiceInterface): ServiceInterface{
     override fun getJsonArray(path: String, params: JSONArray, completionHandler: (response: JSONArray?) -> Unit) {
         service.getJsonArray(path, params, completionHandler)
     }
+
+
+
 
     private val service: ServiceInterface = serviceInjection
 
